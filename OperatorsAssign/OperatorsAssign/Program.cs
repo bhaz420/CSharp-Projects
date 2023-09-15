90c1856d5e8f29e3d1145ab884080ad402719050
bhaz420 <127907510+bhaz420@users.noreply.github.com>
2023-09-15 17:44:57 -0600

    Initial commit

---
+++ OperatorsAssign/OperatorsAssign/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EmployeeManagement
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Employee(int id, string firstName, string lastName)
+        {
+            ID = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        // Overloading the == operator to compare Employee objects by ID
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.ID == emp2.ID;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return emp1.ID != emp2.ID;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate two Employee objects and assign values to their properties
+            Employee employee1 = new Employee(1, "John", "Doe");
+            Employee employee2 = new Employee(2, "Jane", "Smith");
+
+            // Compare the two Employee objects using the overloaded operators
+            bool areEqual = employee1 == employee2;
+            bool areNotEqual = employee1 != employee2;
+
+            // Display the results
+            Console.WriteLine("Are employee1 and employee2 equal? " + areEqual);     // Should print "false"
+            Console.WriteLine("Are employee1 and employee2 not equal? " + areNotEqual); // Should print "true"
+
+            Console.ReadKey(); // Keep the console window open
+        }
+    }
+}
---
+++ Student DataBase Project/Student DataBase Project/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using CodeFirstNewDatabaseSample;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentDbContext())
+            {
+
+                // Add a student
+                var newStudent = new Student
+                {
+                    FirstName = "John",
+                    LastName = "Doe"
+                };
+                context.Students.Add(newStudent);
+                context.SaveChanges();
+
+                // Retrieve and display the added student
+                var addedStudent = context.Students.Find(newStudent.Id);
+                Console.WriteLine($"Added Student: {addedStudent.FirstName} {addedStudent.LastName}");
+            }
+
+        }
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+
+}
